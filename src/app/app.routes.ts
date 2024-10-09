287418e17688bc43164febc96d27b4b8f4bd36da
Gian MIer <giansebas.2015@gmail.com>
2024-10-09 18:42:27 -0500

    Add: Host App MF

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:"",
+    redirectTo: "catalogo-app",
+    pathMatch: "full"
+  },
+  {
+    path: "catalogo-app",
+    loadComponent: ()=>{
+      return loadRemoteModule({
+        remoteEntry: 'http://localhost:4300/remoteEntry.js',
+        remoteName: 'mfe_catalogo',
+        exposedModule: './ProductCatalog'
+      }).then(m => m.AppComponent)
+          .catch(err => console.error(err))
+      ;
+
+    }
+  }
+];
